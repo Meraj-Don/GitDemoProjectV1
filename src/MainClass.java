8373cc844061b97a96d064c1d73b76f9a3e08025
Meraj-Don <merajsgi@gmail.com>
2023-02-20 13:06:40 +0530

    Initial Commit.

---
+++ src/MainClass.java
@@ -0,0 +1,17 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		String hungry="Yes";
+		if(hungry.equals("Yes"))
+		{
+			System.out.print("Go for Lunch");
+		}
+		else
+		{
+			System.out.print("Do Work");
+		}
+
+	}
+
+}
